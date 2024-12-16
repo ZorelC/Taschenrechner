6a06afe438fe3536f1b6787dda27e02af78493c9
Cem Zorel <zorelc@o365-bfw.koeln>
2024-12-16 11:06:19 +0100

    commit test

--- src/calculator/CalculatorTest.java
+++ src/calculator/CalculatorTest.java
@@ -1,5 +1,12 @@
 package calculator;
 
 public class CalculatorTest {
+	public static void main(String[] args) {
+		Calculator calculator = new Calculator();
 
+		System.out.println("Addition: " + calculator.add(5, 3));
+		System.out.println("Subtraktion: " + calculator.subtract(5, 3));
+		System.out.println("Multiplikation: " + calculator.multiply(5, 3));
+		System.out.println("Division: " + calculator.divide(10, 2));
+	}
 }
